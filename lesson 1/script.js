1564879d7273438c3fdf5dba54a456a30bcb8f67
Adizbek Murodov <adiz0777murodov@gmail.com>
2025-12-10 21:52:31 +0500

    Add files via upload

---
+++ lesson 1/script.js
@@ -0,0 +1,49 @@
+// alert('Hello')
+
+// const isAccess = confirm('Are you above 18')
+
+//const name = prompt('What`s your name?')
+//console.log(name)
+//const age = prompt('How old are you?')
+
+
+/* Interpolatsiya
+  const firstName = prompt(' What`s your name?')
+  const lastName = prompt(' What`s your Last Name?')
+  const age = prompt('How old are you?')
+  const phone = prompt(' What`s your phone number? ')
+  const location = prompt(' Where do you live?')
+  console.log('Your fullname is ' + firstName +  lastname +'.You are '+age+'years old
+  '+'.Your number is '+phone+'.Now you live in'+location)
+*/
+/* + belgisi ishlatmasdan console.log qilish
+
+  console.log('My fullname is ${firstName} ${lastName} ')
+*/
+
+/*
+Operatorlar
+
+let x = 6
+let y = 3
+
+Increment xar doim sonni bittaga oshiradi
+Decrement esa xar doim sonni bittaga kamaytiradi
+
+x++
+y--
+
+*/
+
+/*
+&& va -  qachonki true qaytaradi hamma malumotlar true bo`lsa
+|| yokida - qachonki bitta malumot  true bo`lsa
+
+ const isMarried = false
+ const isHaveChild = false
+
+ const isFamily = isMarried || isHaveChild
+ console.log(isFamily)
+*/
+
+//1 hour .55 minute
